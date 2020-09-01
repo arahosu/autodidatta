db9362786efcc135015109a2b81498b2967dec1d
Olivia Gallupová <olivia.gallup@gmail.com>
2020-09-01 10:25:52 +0100

    Finishing base model resnet, still have to correct dimensions

--- self_supervised/TF2/models/byol/byol_pretrain.py
+++ self_supervised/TF2/models/byol/byol_pretrain.py
@@ -4,14 +4,19 @@ import tensorflow.keras.layers as tfkl
 class BYOL(tf.keras.Model):
 
     def __init__(self):
+        pass
     
     def call(self, x, training=False):
+        pass
         
     def shared_step(self, data, training):
+        pass
 
     def train_step(self, data):
+        pass
     
     def test_step(self, data):
+        pass
 
 def main():
     from absl import flags
--- self_supervised/TF2/run.py
+++ self_supervised/TF2/run.py
@@ -1,34 +1,45 @@
 import tensorflow as tf
 import numpy as np
 
-from self_supervised.TF2.model.basemodel import Resnet3DBuilder
+import sys
+# Yes this is awful but it lets modules from sibling directories be imported  https://docs.python.org/3/tutorial/modules.html#the-module-search-path
+sys.path.insert(0,'./models') # 
+sys.path.insert(0,'./') # 
+print('sys.path', sys.path)
+
+from models.basemodel import ResnetBuilder
+# import models.basemodel as basemodel
 
 SHAPE = (32,32,32,8)
 
 
 def load_data():
     toy = np.random.rand(32,32,32,8)
-    toy_samples = tf.data.Dataset.from_tensor_slices([tf.constant(toy) for i in range(5)])
-    toy_labels = tf.data.Dataset.from_tensor_slices([tf.constant(toy) for i in range(5)])
+    toy_samples = tf.data.Dataset.from_tensors([tf.constant(toy) for i in range(5)])
+    toy_labels = tf.data.Dataset.from_tensors([tf.constant(toy) for i in range(5)])
     dataset = tf.data.Dataset.zip((toy_samples, toy_labels))
     return (toy_samples, toy_labels)
 
 
-def make_backbone(input_spec: tf.TensorSpec, output_spec: tf.TensorSpec):
+def make_backbone(input_shape: tf.TensorShape, 
+                  output_shape: tf.TensorShape,
+                  batch_size=None):
     # input_tensor = tf.keras.Input(shape=element_spec.shape[0:-1])
     # return tf.keras.applications.ResNet50(
     #     include_top=False, weights=None, input_tensor=input_tensor, input_shape=element_spec.shape[0:-1],
     #     pooling=None, classes=element_spec.shape[-1]
     # )
-    return Resnet3DBuilder.build_resnet_50(
-                input_shape=element_spec.shape,
-                num_outputs=len(output_spec.shape),
+    return ResnetBuilder.build_resnet_50(
+                input_shape=input_shape,
+                num_outputs=output_shape,  #len(output_shape),
+                batch_size=batch_size,
                 reg_factor=1e-4)
 
 
-def create_model(input_spec: tf.TensorSpec, 
-                 output_spec: tf.TensorSpec):
-    backbone = make_backbone(input_spec, output_spec)
+def create_model(input_shape: tf.TensorShape,
+                 output_shape: tf.TensorShape,
+                 batch_size=None):
+    backbone = make_backbone(input_shape, output_shape, batch_size)
     # lr_rate = LearningRateSchedule()
 
     # model_fn, model_args = select_model()
@@ -37,7 +48,10 @@ def create_model(input_spec: tf.TensorSpec,
     # with strategy.scope():
     #     model = model_fn(*model_args)
     #     model.compile
-    model.compile()
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
 
     # return model
     return model 
@@ -57,7 +71,7 @@ def simclr():
     # self.output_layer = define necessary steps, objective fxn 
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # specify parameters
     # strategy = setup_accelerator()
 
@@ -65,9 +79,11 @@ if __name__=="__main__":
     data = load_data()  #aug_options)
     
     # specify model
+    batch_size = data[0].element_spec.shape[0]
     model = create_model(
-                input_spec=data[0].element_spec, 
-                output_spec=data[1].element_spec)  #model_opts)
+                input_shape=data[0].element_spec.shape[1:], 
+                output_shape=data[1].element_spec.shape[1:],
+                batch_size=batch_size)  #model_opts)
 
     callbacks = tf.keras.callbacks.TensorBoard()
 
@@ -75,7 +91,7 @@ if __name__=="__main__":
     is_train = True
     if is_train:  # FLAGS.is_train:
         history = model.fit(
-            x=data[0], y=data[1], batch_size=1, epochs=5, callbacks=callbacks, steps_per_epoch=4)
+            x=tf.data.Dataset.zip((data[0], data[1])), batch_size=batch_size, epochs=5, callbacks=callbacks, steps_per_epoch=4)
 
     else:  # lif not FLAGS.is_train:
         model.load_weights()
