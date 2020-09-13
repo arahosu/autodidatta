27ffd3e14575aee632f09d9605d5158b1872a1ce
Olivia Gallupová <olivia.gallup@gmail.com>
2020-09-13 16:07:05 +0100

    Problems with l2-normalized mse loss for BYOL. Using JAX pseudocode to try and fix

--- self_supervised/TF2/models/byol/byol_pretrain.py
+++ self_supervised/TF2/models/byol/byol_pretrain.py
@@ -1,10 +1,96 @@
 import tensorflow as tf
 import tensorflow.keras.layers as tfkl
+from tensorflow_addons.optimizers import LAMB
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+
+# JUST FOR DEBUGGING ON VSCODE 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+parentdir = os.path.dirname(parentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0,parentdir) 
+
+from self_supervised.TF2.models.networks.resnet50 import ResNet50
+from self_supervised.TF2.utils.losses import mse_loss
+
+from self_supervised.TF2.models.simclr.simclr_flags import FLAGS
+from absl import app
+
+
+class EMA():
+    """ Exponential moving average """
+    def __init__(self, tau):
+        super().__init__()
+        self.tau = tau
+
+    def update_average(self, old, new):
+        if old is None:
+            return new
+        return old * self.tau + (1 - self.tau) * new
+
+
+def update_moving_average(ema_updater, ma_model, current_model):
+    for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
+        old_weight, up_weight = ma_params.data, current_params.data
+        ma_params.data = ema_updater.update_average(old_weight, up_weight)
+
+
+# class NetWrapper(tf.keras.Model):
+#     """ Initializes the online network """
+
+#     def __init__(self,
+#         net: tf.keras.Model,
+#         projection_size,
+#         projection_hidden_size,
+#         layer=hidden_layer):
+#         super(NetWrapper, self).__init__()
+#         self.net = net
+
+
+class MLP(tf.keras.Sequential):
+    """ MLP class for projector and predictor """
+    def __init__(self, in_shape):
+        super(MLP, self).__init__()
+
+        hidden_size = int(np.prod(in_shape))
+        projection_size = int(np.prod(in_shape))
+
+        self.add(tf.keras.Input(shape=in_shape))
+        self.add(tf.keras.layers.Flatten())
+        self.add(tf.keras.layers.Dense(hidden_size))
+        self.add(tf.keras.layers.BatchNormalization())
+        self.add(tf.keras.layers.ReLU())
+        self.add(tf.keras.layers.Dense(projection_size))
+        # self.net = tf.keras.Sequential(layers=[
+        #     tf.keras.Input(shape=in_shape),
+        #     tf.keras.layers.Dense(hidden_size),  #, activation='linear'),  # TODO: check if equivalent to nn.Linear()
+        #     tf.keras.layers.BatchNormalization(),
+        #     tf.keras.layers.ReLU(),
+        #     tf.keras.layers.Dense(projection_size)  #, activation='linear')
+        # ])
+
+    def call(self, x, training=True):
+        # return self.net(x, training=training)
+
+        return super(MLP, self).call(x, training=training)
+
 
 class BYOL(tf.keras.Model):
 
-    def __init__(self):
-        pass
+    def __init__(self, backbone):
+        self.online_encoder = backbone
+        self.projection = MLP()
+        self.predictor = MLP()
+        self.online_network = tf.keras.Sequential(
+            self.online_encoder,  # base encoder f, outputs feature space y as (8*img dims)
+            self.projection,  # MLP projection g, maps feature space onto 
+        )  # NetWrapper()
+        self.online_predictor = MLP()
+        self.target_encoder = None
+        self.target_ema = EMA()
     
     def call(self, x, training=False):
         pass
@@ -13,13 +99,103 @@ class BYOL(tf.keras.Model):
         pass
 
     def train_step(self, data):
+        # apply gradient tape to online network only
+
+        # loss = byol_loss_fn()
         pass
 
     def test_step(self, data):
         pass
 
-def main():
-    from absl import flags
+    def update_moving_average(self):
+        assert self.target_encoder is not None, 'target encoder has not been created yet'
+        update_moving_average(self.target_ema_updater, self.target_encoder, self.online_encoder)
+
+
+def main(argv):
+    del argv
+    # from absl import flags
+
+    # generate fake data
+    # x = np.random.randint(0, 8, size=(100, 2, 3, 3, 4))
+    # y = np.random.randint(0, 8, size=(100, 2, 3, 3, 4))
+
+    # tf.random.normal((3, 2, 3, 3, 4)))
+
+    # # y = np.random.choice([0, 1], size=(1000,))
+    # # y = tf.keras.utils.to_categorical(y, 8)
+    # x = x.astype('float32')
+    # y = y.astype('float32')
+
+    # Set up accelerator
+    # strategy = setup_accelerator(FLAGS.use_gpu,
+    #                              FLAGS.num_cores,
+    #                              FLAGS.tpu)
+    global_batch_size = FLAGS.num_cores * FLAGS.batch_size
+
+    if FLAGS.dataset == 'cifar10':
+        from self_supervised.TF2.dataset.cifar10 import load_input_fn
+        import tensorflow_datasets as tfds
+
+        train_ds = load_input_fn(split=tfds.Split.TRAIN,
+                                 name='cifar10',
+                                 batch_size=FLAGS.batch_size,
+                                 training_mode='pretrain')
+
+        val_ds = load_input_fn(split=tfds.Split.TEST,
+                               name='cifar10',
+                               batch_size=FLAGS.batch_size,
+                               training_mode='pretrain')
+
+        ds_info = tfds.builder(FLAGS.dataset).info
+        steps_per_epoch = ds_info.splits['train'].num_examples // global_batch_size
+        validation_steps = ds_info.splits['test'].num_examples // global_batch_size
+        ds_shape = (32, 32, 6)
+
+    # if FLAGS.backbone == 'resnet50':
+
+    #     backbone = ResNet50(include_top=False,
+    #                         input_shape=ds_shape,
+    #                         pooling=None)
+
+    # with strategy.scope():
+    # load model
+    # model = MLP(ds_shape)
+
+    in_shape = ds_shape
+
+    hidden_size = int(np.prod(in_shape))
+    projection_size = int(np.prod(in_shape))
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.Input(shape=in_shape))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(hidden_size))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.ReLU())
+    model.add(tf.keras.layers.Dense(projection_size))
+
+
+    if FLAGS.optimizer == 'lamb':
+        optimizer = LAMB(learning_rate=FLAGS.learning_rate)
+    elif FLAGS.optimizer == 'adam':
+        optimizer = Adam(lr=FLAGS.learning_rate)
+
+    # tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer=optimizer, loss=mse_loss, metrics=['accuracy'])
+    model.build((None, *ds_shape))
+    model.summary()
+
+    # build model and compile it
+    history = model.fit(train_ds,
+              steps_per_epoch=steps_per_epoch,
+              batch_size=global_batch_size,
+              epochs=10,
+              validation_data=val_ds,
+              validation_steps=validation_steps)
+
+    print(history)
+
 
 if __name__ == '__main__':
-    main()
+    app.run(main)
