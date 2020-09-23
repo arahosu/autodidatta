3b5285f655c7ced1a9b07dd1588c8474091ba5c5
arahosu <arahosu@gmail.com>
2020-09-23 19:24:17 +0000

    added an option to set whether to apply color jitter transformations randomly or not

--- self_supervised/TF2/models/simclr/simclr_transforms.py
+++ self_supervised/TF2/models/simclr/simclr_transforms.py
@@ -22,18 +22,108 @@ def random_brightness(image, max_delta, impl='v1'):
 
     return image
 
-def color_jitter(image, strength=1.0, impl='v1'):
+def color_jitter(image, strength=1.0, random_order=True, impl='v1'):
 
     brightness = 0.8 * strength
     contrast = 0.8 * strength
     saturation = 0.8 * strength
     hue = 0.2 * strength
 
-    image = random_brightness(image, brightness, impl=impl) 
-    image = tf.image.random_contrast(image, 1 - contrast, 1 + contrast)
-    image = tf.image.random_saturation(image, 1 - saturation, 1 + saturation)
-    image = tf.image.random_hue(image, hue)
+    if random_order:
+        return color_jitter_rand(image, brightness, contrast, saturation, hue, impl=impl)
+    else:
+        return color_jitter_nonrand(image, brightness, contrast, saturation, hue, impl=impl)
 
+def color_jitter_nonrand(image,
+                         brightness=0,
+                         contrast=0,
+                         saturation=0,
+                         hue=0,
+                         impl='v1'):
+    """Distorts the color of the image (jittering order is fixed).
+    Args:
+    image: The input image tensor.
+    brightness: A float, specifying the brightness for color jitter.
+    contrast: A float, specifying the contrast for color jitter.
+    saturation: A float, specifying the saturation for color jitter.
+    hue: A float, specifying the hue for color jitter.
+    impl: 'v1' or 'v2'.  Whether to use simclrv1 or simclrv2's
+        version of random brightness.
+    Returns:
+    The distorted image tensor.
+    """
+    def apply_transform(i, x, brightness, contrast, saturation, hue):
+        """Apply the i-th transformation."""
+        if brightness != 0 and i == 0:
+            x = random_brightness(x, max_delta=brightness, impl=impl)
+        elif contrast != 0 and i == 1:
+            x = tf.image.random_contrast(
+                x, lower=1 - contrast, upper=1 + contrast)
+        elif saturation != 0 and i == 2:
+            x = tf.image.random_saturation(
+                x, lower=1 - saturation, upper=1 + saturation)
+        elif hue != 0:
+            x = tf.image.random_hue(x, max_delta=hue)
+        return x
+
+    for i in range(4):
+        image = apply_transform(i, image, brightness, contrast, saturation, hue)
+        image = tf.clip_by_value(image, 0., 1.)
+    return image
+
+def color_jitter_rand(image,
+                      brightness=0,
+                      contrast=0,
+                      saturation=0,
+                      hue=0,
+                      impl='v1'):
+    """Distorts the color of the image (jittering order is random).
+    Args:
+    image: The input image tensor.
+    brightness: A float, specifying the brightness for color jitter.
+    contrast: A float, specifying the contrast for color jitter.
+    saturation: A float, specifying the saturation for color jitter.
+    hue: A float, specifying the hue for color jitter.
+    impl: 'v1' or 'v2'.  Whether to use simclrv1 or simclrv2's
+        version of random brightness.
+    Returns:
+    The distorted image tensor.
+    """
+    def apply_transform(i, x):
+        """Apply the i-th transformation."""
+        def brightness_foo():
+            if brightness == 0:
+                return x
+            else:
+                return random_brightness(x, max_delta=brightness, impl=impl)
+
+        def contrast_foo():
+            if contrast == 0:
+                return x
+            else:
+                return tf.image.random_contrast(x, lower=1 - contrast, upper=1 + contrast)
+
+        def saturation_foo():
+            if saturation == 0:
+                return x
+            else:
+                return tf.image.random_saturation(x, lower=1 - saturation, upper=1 + saturation)
+
+        def hue_foo():
+            if hue == 0:
+                return x
+            else:
+                return tf.image.random_hue(x, max_delta=hue)
+
+        x = tf.cond(tf.less(i, 2),
+                    lambda: tf.cond(tf.less(i, 1), brightness_foo, contrast_foo),
+                    lambda: tf.cond(tf.less(i, 3), saturation_foo, hue_foo))
+        return x
+
+    perm = tf.random_shuffle(tf.range(4))
+    for i in range(4):
+        image = apply_transform(perm[i], image)
+        image = tf.clip_by_value(image, 0., 1.)
     return image
 
 def color_drop(image, keep_channels=True):
