01c22890884a2e21162d7cfaf6ba710ad6cc9b33
arahosu <arahosu@gmail.com>
2020-09-23 19:24:17 +0000

    added an option to set whether to apply color jitter transformations randomly or not

--- run_simclr.sh
+++
@@ -1,3 +0,0 @@
-# python3 -m self_supervised.TF2.models.simclr.simclr_pretrain --train_epochs=2000 && 
-python3 -m self_supervised.TF2.models.simclr.simclr_finetune --linear_eval=True --weights=gs://cifar10_baseline/checkpoints/simclr_weights.ckpt &&
-python3 -m self_supervised.TF2.models.simclr.simclr_finetune --linear_eval=False --weights=gs://cifar10_baseline/checkpoints/simclr_weights.ckpt 
